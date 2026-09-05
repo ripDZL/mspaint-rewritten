70c0e8f157fe1e3e668b2ba24282768ad7c2a5c1
ripDZL <Tesonman@protonmail.com>
2026-09-05 14:52:45 -0400

    Enable Delphi leak reporting in debug builds

--- Paint.dpr
+++ Paint.dpr
@@ -12,6 +12,10 @@ uses
 {$R *.res}
 
 begin
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TMsPaint, MsPaint);
